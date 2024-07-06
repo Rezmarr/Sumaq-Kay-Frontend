419bbbbb4b021481af7223fbfc24ddb75c4ad5c1
Renzo Zapata <renzozs2000@gmail.com>
2024-07-06 13:53:19 -0500

    fix(frontend): modifica services para prod

--- src/environments/services/services.ts
+++ src/environments/services/services.ts
@@ -1,9 +1,11 @@
 export const SERVICES = Object.freeze({
     apiGateway: {
-      url: 'http://localhost:3000/api-gateway',
+      // url: 'http://localhost:3000/api-gateway',
+      url: 'https://sumaq-kay-api.vercel.app/api-gateway'
     },
     frontend: {
-      url: 'http://localhost:4200/#',
+      // url: 'http://localhost:4200/#',
+      url: 'https://sumaq-kay-frontend.vercel.app',
     },
   });
   
